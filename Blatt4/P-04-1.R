999d35fe0f970d78ab647299b410e5d3d4864f21
Paul Proft <paul.proft@stud.uni-heidelberg.de>
2025-11-11 17:45:22 +0100

    Added Files via GitHub page

---
+++ Blatt4/P-04-1.R
@@ -0,0 +1,3 @@
+s <- "Three principles underlying R (according to Chambers): 
+Everything that exists in R is an object; everything that 
+happens in R is a function call; interfaces to other software are part of R."
---
+++ Blatt4/P-04-2.R
@@ -0,0 +1,5 @@
+inputDates <- list("13.05.2024", "3.12.23", "2. September 2012", 
+                   "21/5, 2024", "Mi., 7. Aug. 2024")
+
+monthsDE <- c("Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", 
+              "August", "September", "Oktober", "November", "Dezember")
