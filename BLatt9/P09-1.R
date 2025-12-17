1d69804d94dc968489139d74565dcaf527fa7df1
Paul P <paul.proft@stud.uni-heidelberg.de>
2025-12-17 19:21:40 +0100

    9-1 done

--- BLatt9/P09-1.R
+++ BLatt9/P09-1.R
@@ -1,2 +1,15 @@
 # Paul Proft, Lionel Assick, Gina Lilienkamp, Emma Roser
 
+`%->%` <- function(lhs, rhs) {
+  assign(deparse(substitute(rhs)), lhs, envir = parent.frame())
+  rhs
+}
+
+
+#test
+
+c(4,5) %->% x1 |>
+sum() %->% x2 |>
+sqrt() -> res
+
+cat(x1,"\n", x2,"\n", res,"\n")
